b29a15f76a5891f7c0e060e839d73741528add75
LyleW473 <118493502+LyleW473@users.noreply.github.com>
2022-11-19 22:43:42 +0000

    Adding useful tricks file

---
+++ Useful tricks.py
@@ -0,0 +1,45 @@
+# To comment out a large block of code at once use: Ctrl + / 
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+#print(test_variable)
+
+# You can also use ''' to explain an important secion
+''' 
+
+This is a really important section
+
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+test_variable = "Hello"
+print("testing")
+
+'''
+
+# Declare two or more variables with a single line
+
+a = 10; b = 20
+print(a)
+print(b)
+
+c = 30; d = 40; e = 50; f = 60; g = 70; 
+print(c,d,e,f,g)
+
+# Separate a long single line of code into two lines, with Python still interpreting it as a single logical line
+h = 1 + 2 + 3 + 4 + 5 + 6 + 7 \
++ 8 + 9 + 10
+
+print(h)
+
+
+# You can create a tuple without actually needing brackets
+a_list = (1,2,3)
+# is the same as
+a_list = 1,2,3
