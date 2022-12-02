a0dcd48d2383bfaf97df286d4816229587756d45
LyleW473 <118493502+LyleW473@users.noreply.github.com>
2022-12-02 22:58:14 +0000

    Adding files on modules.

---
+++ modules.py
@@ -0,0 +1,14 @@
+import string
+from datetime import datetime as dt
+
+"""
+- Modules are extra parts that we can attach to our programs. These can be from the Python Standard Library or those made from other people.
+- We can also create our own modules (across multiple files) to make code more organised
+
+- Python Standard Library: https://docs.python.org/3/library/
+
+"""
+
+print(string.ascii_lowercase)
+
+print(dt.now())
